084db03b4fcf9f090b8ae6d9ccc478e210b616d7
javivega <marribas44@gmail.com>
2017-05-14 13:47:36 +0200

    Final React Router Setup Inicio Helpers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { BrowserRouter, Match, Miss } from 'react-router'
 import App from './components/App'
 import './css/style.css'
+import StorePicker from './components/StorePicker'
+import NotFound from './components/NotFound'
 
 
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
 
-render(<App />, document.querySelector('#main'));
+render(<Root />, document.querySelector('#main'));
